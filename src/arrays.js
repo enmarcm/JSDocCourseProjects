1354df222527ddb9b2c954d9aee4b48610e56bc4
enmarcm <theenmanuel123@gmail.com>
2023-09-10 22:19:23 -0400

    Nuevos archivos

---
+++ src/arrays.js
@@ -0,0 +1,13 @@
+/**
+ * Lista de Edades de usuarios
+ * @type {Array<number>} 
+ */
+const age = [10, 20, 30, 100];
+
+
+/**
+ * Lista de puntaje de los usuarios
+ * @type {Array<number | string>}
+ */
+const puntaje = [13, '13', 16.4, '40']
+
---
+++ src/objects.js
@@ -0,0 +1,28 @@
+//* ESTA ES LA MANERA DEL VIDEO
+/** 
+ * Persona Muestra
+ * @type {{id: number, firstName: string, lastName: string, age: number}}
+ */
+const persona = {
+    id: 1,
+    firstName: 'Enmanuel',
+    lastName: 'Colina',
+    age: 20
+}
+
+
+//* ESTA ES LA MANERA MIA
+//* No funciona porque no detecta bien las cosas
+// /**
+//  * @type {object}
+//  * @prop {number} id - Id del usuario
+//  * @prop {string} firstName - Nombre del usuario
+//  * @prop {string} lastName - Apellido del usuario
+//  * @prop {number} age - Edad del Usuario
+//  */
+// const person = {
+//     id: 1,
+//     firstName: 'Enmanuel',
+//     lastName: 'Colina',
+//     age: 20
+// }
